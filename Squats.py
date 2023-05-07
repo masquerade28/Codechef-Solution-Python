56cc6648467e20ae3c533e656dc394e99f93b4ea
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-07 10:13:08 +0530

    Create Squats.py

---
+++ Squats.py
@@ -0,0 +1,12 @@
+'''
+  Somu went to the gym today. He decided to do X sets of squats. Each set consists of 15 squats. Determine the total number of squats that he did today.
+'''
+
+# Running Loop For Test Cases
+for t in range(int(input())):
+  
+  # Taking Input For Number Of Sets
+  x = int(input())
+  
+  # Printing Number Of Squats He Did
+  print(15*x)
